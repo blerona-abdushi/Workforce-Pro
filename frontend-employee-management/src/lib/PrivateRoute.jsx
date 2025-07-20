a0a4fa8b5bc26f84940cf8f4f99582f27213c18f
blerona-abdushi <bleronaabdushi@gmail.com>
2025-07-20 13:11:31 -0700

    initial commit

--- frontend-employee-management/src/lib/PrivateRoute.jsx
+++ frontend-employee-management/src/lib/PrivateRoute.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 import { Navigate } from 'react-router-dom';
 
-const PrivateRoute = ({children}) => {
+const PrivateRoute = ({ children }) => {
     const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
-    if(!isAuthenticated){
-        return <Navigate to="login" />;
+
+    if (!isAuthenticated) {
+        return <Navigate to="/login" />;
     }
 
-  return children
+    return children;
 }
 
-export default PrivateRoute
+export default PrivateRoute;
